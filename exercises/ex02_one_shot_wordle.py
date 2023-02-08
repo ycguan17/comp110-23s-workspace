c0f4d02733105e9cc83755bcf1d9a67b4b744da9
Carolyn Guan <ycguan@ad.unc.edu>
2023-02-08 00:07:44 -0500

    progress

--- exercises/ex02_one_shot_wordle.py
+++ exercises/ex02_one_shot_wordle.py
@@ -1,9 +1,9 @@
-"""EX02 - One-Shot Wordle"""
+"""EX02 - Attempts at a One-Shot Wordle."""
 
-__author__: "730372605"
+__author__ = "730372605"
 
 secret: str = "python"
-guess: str = input("What is your " + str(len(secret)) +"-letter guess? ")
+guess: str = input("What is your " + str(len(secret)) + "-letter guess? ")
 WHITE_BOX: str = "\U00002B1C"
 GREEN_BOX: str = "\U0001F7E9"
 YELLOW_BOX: str = "\U0001F7E8"
@@ -11,26 +11,24 @@ index: str = ""
 check: int = 0
 char_found: bool = False
 char_index: int = 0
-
 while (len(guess) != len(secret)):
-    guess: str = input("That was not " + str(len(secret)) + " letters! Try again: ")
+    guess = input("That was not " + str(len(secret)) + " letters! Try again: ")
 while check < len(secret):
     if secret[check] == guess[check]:
         index = index + GREEN_BOX
     else:
-        char_index: int = 0
-        char_found: bool = False
+        char_index = 0
+        char_found = False
         while not char_found and char_index < len(secret):
             if secret[char_index] == guess[check]:
-                char_found: bool = True
+                char_found = True
             char_index = char_index + 1
-        if char_found == True:
+        if bool(char_found) is True:
             index = index + YELLOW_BOX
         else:
             index = index + WHITE_BOX
     check = check + 1
 print(index)
-
 if (guess == secret):
     print("Woo. You got it! ")
 else:
