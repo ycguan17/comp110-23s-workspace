61e2b3fbada43960d27d677f56abda29d434ea02
Carolyn Guan <ycguan@ad.unc.edu>
2023-02-22 14:05:18 -0500

    2

--- exercises/ex03_wordle.py
+++ exercises/ex03_wordle.py
@@ -48,7 +48,7 @@ def main() -> None:
     secret: str = "codes"
     current_round: int = 1
     while current_round <= 6:
-        print("=== Turn " + str(current_round) + "/6" + " ===")
+        print("=== Turn " + str(current_round) + "/6 ===")
         guess: str = input_guess(len(secret))
         print(emojified (guess, secret))
         if guess == secret:  
@@ -57,7 +57,7 @@ def main() -> None:
         current_round = current_round + 1
     else:
         if guess != secret and current_round > 6:
-            print("X/6 Sorry, try again tomorrow!")
+            print("X/6 - Sorry, try again tomorrow!")
 
 
 if __name__ == "__main__":
