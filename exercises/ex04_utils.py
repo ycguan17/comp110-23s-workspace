294a467b3b62b83e7e7b0d005730cc6edd6da1af
Carolyn Guan <ycguan@ad.unc.edu>
2023-02-28 22:56:24 -0500

    1

---
+++ exercises/ex04_utils.py
@@ -0,0 +1,51 @@
+"""List Utility Functions."""
+
+__author__ : "730372605"
+
+
+def all(intList: list[int], num: int ) -> bool:
+    """Given a list of ints and a int, returns bool for if the list and int are the same."""
+    idx: int = 0
+    while idx < len(intList):
+        if num != intList[idx]:
+            return False
+        idx += 1
+    return True
+
+def max(intList: list[int]) -> int:
+    if len(intList) == 0:
+        raise ValueError("max() arg is an empty List")
+    idx: int = 0
+    maxNum: int = intList[ind]
+    while idx < len(intList):
+        if maxNum < intList[idx]:
+            maxNum = intList [idx]
+        idx += 1
+    return maxNum
+
+def is_equal(list1: list[int], list2: list[int]) -> bool:
+    idx: int = 0
+    if len(list1) != len(list2):
+        return False
+    while idx < len(list1) and idx < len(list2):
+        if list1[idx] != list2[idx]:
+            return False
+        idx += 1
+    return True
+
+def test():
+    print("----Testing ALL function----")
+    print(all([1, 2, 3], 1))
+    print(all([1, 1, 1], 2))
+    print(all([1, 1, 1], 1))
+
+    print("----Testing MAX function----")
+    print(max([1, 3, 2]))
+    print(max([100, 99, 98]))
+    #print(max([]))
+
+    print("----Testing IS_EQUAL function----")
+    print(is_equal([1, 0, 1],[1, 0, 1]))
+    print(is_equal([1, 0, 1],[1, 0, 1]))
+
+test()
