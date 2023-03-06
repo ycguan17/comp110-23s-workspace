5228f58058fde1b0c41fbec54564f2de32a49ae3
Carolyn Guan <ycguan@ad.unc.edu>
2023-03-05 21:02:24 -0500

    1

---
+++ lessons/sum.py
@@ -0,0 +1,8 @@
+"""Example function for unit tests"""
+
+def sum(xs: list[float]) -> float:
+    """return sum off all elements in xs"""
+    sum_total: float = 0.0
+    for elem in xs:
+        sum_total += elem
+    return sum_total
