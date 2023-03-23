1dee107f298ec8d6a4a6df37727ce6a520e34de0
Carolyn Guan <ycguan@ad.unc.edu>
2023-03-22 23:34:01 -0400

    save plz

--- exercises/cyoa.py
+++ exercises/cyoa.py
@@ -17,6 +17,7 @@ def secret() -> None:
 
 def greet(name: str) -> None:
     """Asks for name and gives summary and greetings for the game."""
+    global player
     player = input(player)
     print(f"Welcome to the Escape Game, {name}. Currently you are trapped within a mansion. All the doors are locked except for the kitchen, the bedroom and the basement. In order to escape, you must find the corrrect code to open the main door. P.S. Keep in mind that you start off with 100 adventure points, which you lose with each wrong combination given. Each wrong combo subtracts 20 points. P.P.S. Try inputting the game name for a surprise!~")
 
