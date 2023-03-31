87a5d6244fb1e2877f2ddac0a4d2e9b697d9d570
Carolyn Guan <ycguan@ad.unc.edu>
2023-03-30 20:03:28 -0400

    work

--- exercises/ex07/dictionary.py
+++ exercises/ex07/dictionary.py
@@ -1,10 +1,11 @@
 """Dictionary functions."""
 
-__author__ : "730372605"
+__author__: "730372605"
 
 
 def invert(initial: dict[str, str]) -> dict[str, str]:
-    new : dict[str, str] = {}
+    """Flips values and keys within dictionary."""
+    new: dict[str, str] = {}
     for key in initial.keys():
         if initial[key] in new.keys():
             raise KeyError("Repeat keys/values")
@@ -12,14 +13,15 @@ def invert(initial: dict[str, str]) -> dict[str, str]:
     return new
 
 
-def favorite_color(initial_dict: dict[str, str]) -> str:
-    color : dict[str, str] = {}
-    for item in initial_dict.keys():
-        if color.has_key(initial_dict[item]):
-            color[initial_dict[item]] += 1
+def favorite_color(initial: dict[str, str]) -> str:
+    """Finds the most frequent key in dictionary."""
+    color: dict[str, str] = {}
+    for item in initial.keys():
+        if initial[item] in color.keys():
+            color[initial[item]] += 1
         else:
-            color[initial_dict[item]] = 1
-    maxColor : str = color.keys()[0]
+            color[initial[item]] = 1
+    maxColor: str = color.keys()[0]
     for item in color.keys():
         if color[maxColor] < color[item]:
             maxColor = item
@@ -27,7 +29,8 @@ def favorite_color(initial_dict: dict[str, str]) -> str:
 
 
 def count(input: list[str]) -> dict[str, int]:
-    newDic : dict[str, int] = {}
+    """Count the unique elements of the list into a dictionary."""
+    newDic: dict[str, int] = {}
     for item in input:
         if item in newDic.keys:
             newDic[item] += 1
